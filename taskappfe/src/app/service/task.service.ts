d85d5dec335e24f831814c280f48b52b5173efd9
SOWBARNIGAA SHANMUGAVADIVEL <85994406+Sowbarnigaa@users.noreply.github.com>
2024-02-08 11:29:06 +0530

    Update task.service.ts

--- taskappfe/src/app/service/task.service.ts
+++ taskappfe/src/app/service/task.service.ts
@@ -11,28 +11,28 @@ export class TaskService {
 
   postTask( task:any):Observable<any>
   {
-    return this.http.post(BASIC_URL+"/api/insert",task);
+    return this.http.post(BASIC_URL+"/tasks/insert",task);
   }
   getAllTask(): Observable<any>
   {
-    return this.http.get(BASIC_URL+"/api/findAll")
+    return this.http.get(BASIC_URL+"/tasks/findAll")
   }
   getTaskById(id:number):Observable<any>
   {
-    return this.http.get(BASIC_URL+"/api/update/"+id)
+    return this.http.get(BASIC_URL+"/tasks/update/"+id)
   }
 
   updateTask(id:number,task:any):Observable<any>
   {
-    return this.http.put(BASIC_URL+"/api/update/"+id,task)
+    return this.http.put(BASIC_URL+"/tasks/update/"+id,task)
   }
   deleteTask(id:number):Observable<any>
   {
-    return this.http.delete(BASIC_URL+"/api/update/"+id)
+    return this.http.delete(BASIC_URL+"/tasks/update/"+id)
   }
   searchTask(query: string): Observable<any> 
   {
-    return this.http.get(BASIC_URL+"/api/match/"+query)
+    return this.http.get(BASIC_URL+"/tasks/match/"+query)
   }
 
 }
